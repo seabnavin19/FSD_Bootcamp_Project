d913cb4d4f69ae91d39a3aef9bb5c73b15059d92
Yeansovanvathana <61142466+Yeansovanvathana@users.noreply.github.com>
2020-09-30 14:52:51 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const Information = require('./routes/api/Information');
 
 app.use('/api/MajorsInfo', Information);
 
+//Port 4000
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
