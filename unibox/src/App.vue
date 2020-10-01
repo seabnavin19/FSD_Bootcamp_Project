0a45af4f980d5b5559157546d708d46ebb9944e7
Chanpiseth Lim <limchanpiseth@gmail.com>
2020-10-01 12:33:11 +0700

    made some changes to Vue component

--- unibox/src/App.vue
+++ unibox/src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
-    <UniInfo/>
+    <Navbar/>
+      <UniInfo/>
+    <Footer/>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import UniInfo from './pages/UniInfo'
 
 export default {
   name: 'App',
   components: {
-    UniInfo
+    UniInfo,
+    Navbar,
+    Footer
   }
 }
 </script>
