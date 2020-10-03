a7d05ee4666ce8a0ae18ae580a04efb0470f1a04
Chanpiseth Lim <limchanpiseth@gmail.com>
2020-10-03 18:57:38 +0700

    a

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(__dirname + '/public/'))
 
     //Handle SPA
-    app.get(/.*/, (req, res) => res.sendFile(path.resolve(__dirname + '/public/index.html')))
+    app.get(/.*/, (req, res) => res.sendFile(path.join(__dirname , '/public/index.html')))
 
 }
 
